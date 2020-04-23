aeba6625ef5ffbf138c5c336c531099b81d40d8f
izzy <542001917@qq.com>
2020-04-23 20:07:46 +0800

    chore: install iview

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import iView from "iview";
+import "iview/dist/styles/iview.css"; // 使用 CSS
+
+Vue.use(iView);
 
 Vue.config.productionTip = false;
 
