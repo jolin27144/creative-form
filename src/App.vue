b1d9a08f5c512623b39b602a7dcf41559e290032
runxiongliang <542001917@qq.com>
2020-05-07 17:47:38 +0800

    refactor: 增加默认选项

--- src/App.vue
+++ src/App.vue
@@ -33,133 +33,76 @@ export default {
         description: "这是描述描述",
         form: [
           {
-            ele: "signature",
+            ele: "radio",
+            default: true,
             obj: {
-              type: "signature",
-              config: true,
-              label: "签名",
-              img: "",
-              showEsign: false,
+              type: "radio",
+              config: false,
+              label: "是否需要镇街核实企业整改",
               inlineBlock: false,
               require: true,
               value: "",
               items: [
-                { label_value: true, label_name: "是" },
-                { label_value: false, label_name: "否" }
-              ],
-              name: "签名0",
-              ruleError: "请选择",
-              relation: false,
-              relation_name: "",
-              relation_value: "",
-              visibility: true,
-              index: 1
-            }
-          },
-          {
-            ele: "radio",
-            obj: {
-              type: "radio",
-              config: true,
-              label: "单选",
-              inlineBlock: false,
-              require: true,
-              value: "1",
-              items: [
                 { label_value: "1", label_name: "是" },
                 { label_value: "0", label_name: "否" }
               ],
-              name: "单选1",
+              name: "是否需要镇街核实企业整改0",
               ruleError: "请选择",
               relation: false,
               relation_name: "",
               relation_value: "",
               visibility: true,
-              index: 2
-            }
-          },
-          {
-            ele: "checkbox",
-            obj: {
-              type: "checkbox",
-              config: true,
-              label: "多选",
-              inlineBlock: false,
-              require: true,
-              value: ["2"],
-              items: [
-                { label_value: "1", label_name: "选项1" },
-                { label_value: "2", label_name: "选项2" }
-              ],
-              name: "多选2",
-              ruleError: "该选项不能为空",
-              relation: false,
-              relation_name: "",
-              relation_value: "",
-              visibility: true,
-              index: 3
-            }
-          },
-          {
-            ele: "input",
-            obj: {
-              type: "input",
-              config: true,
-              label: "单行文本",
-              placeholder: "",
-              inlineBlock: false,
-              require: true,
-              maxLength: 400,
-              items: [{ label_value: null, label_name: "" }],
-              value: "dfasfasdfadsfasf",
-              name: "单行文本3",
-              ruleError: "该字段不能为空",
-              relation: false,
-              relation_name: "",
-              relation_value: "",
-              visibility: true,
-              index: 4
-            }
-          },
-          {
-            ele: "text",
-            obj: {
-              type: "textarea",
-              config: true,
-              label: "多行文本",
-              placeholder: "",
-              inlineBlock: false,
-              maxLength: 200,
-              require: true,
-              maxRows: 5,
-              value: "asdfdsafasfsdafasdfsafdsfsfdasf",
-              name: "多行文本4",
-              ruleError: "该字段不能为空",
-              relation: false,
-              relation_name: "",
-              relation_value: "",
-              visibility: true,
-              index: 5
+              index: 1,
+              loading: false,
+              listIndex: 1
             }
           },
           {
             ele: "datepicker",
+            default: true,
             obj: {
               type: "datepicker",
-              config: true,
-              label: "时间控件",
-              placeholder: "",
+              config: false,
+              label: "整改开始时间",
+              placeholder: "请选择整改开始时间",
               inlineBlock: false,
               require: true,
-              name: "时间控件5",
-              value: "2020年04月14日",
+              name: "整改开始时间1",
+              value: "",
               ruleError: "选项不能为空",
               relation: false,
               relation_name: "",
               relation_value: "",
               visibility: true,
               format: "yyyy年MM月dd日",
-              index: 6
+              index: 2,
+              loading: false,
+              listIndex: 2,
+              items: [{ label_value: "1588844338.757", label_name: "" }]
+            }
+          },
+          {
+            ele: "datepicker",
+            default: true,
+            obj: {
+              type: "datepicker",
+              config: false,
+              label: "整改结束时间",
+              placeholder: "请选择整改结束时间",
+              inlineBlock: false,
+              require: true,
+              name: "整改结束时间2",
+              value: "",
+              ruleError: "选项不能为空",
+              relation: false,
+              relation_name: "",
+              relation_value: "",
+              visibility: true,
+              format: "yyyy年MM月dd日",
+              index: 3,
+              loading: false,
+              listIndex: 3,
+              items: [{ label_value: "1588844370.679", label_name: "" }]
             }
           }
         ]
